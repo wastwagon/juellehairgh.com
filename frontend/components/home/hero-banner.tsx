68d8046a37bbdf330ad88cd6e94ebbab1ea1d60d
wastwagon <gilbert.amidu@gmail.com>
2026-01-01 20:28:27 +0000

    feat: increase hero banner size slightly
    - Increased mobile height from 400px to 450px
    - Increased medium height from 500px to 550px
    - Increased large height from 600px to 650px
    - Increased xl height from 650px to 700px
    - Provides more visual impact while maintaining responsiveness

--- frontend/components/home/hero-banner.tsx
+++ frontend/components/home/hero-banner.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export function HeroBanner() {
   return (
-    <section className="relative w-full h-[400px] md:h-[500px] lg:h-[600px] xl:h-[650px] overflow-hidden">
+    <section className="relative w-full h-[450px] md:h-[550px] lg:h-[650px] xl:h-[700px] overflow-hidden">
       {/* Hero Image */}
       <div className="relative w-full h-full flex items-center justify-center bg-gray-100">
         <Image
