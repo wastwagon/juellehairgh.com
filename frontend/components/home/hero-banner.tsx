5ddccf0f111acf71218159f3c7b0da023a05d28d
wastwagon <gilbert.amidu@gmail.com>
2026-01-01 19:24:55 +0000

    Increase hero banner height
    - Increase mobile height from 350px to 400px
    - Increase tablet height from 450px to 500px
    - Increase desktop height from 550px to 600px

--- frontend/components/home/hero-banner.tsx
+++ frontend/components/home/hero-banner.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export function HeroBanner() {
   return (
-    <section className="relative w-full h-[350px] md:h-[450px] lg:h-[550px] overflow-hidden">
+    <section className="relative w-full h-[400px] md:h-[500px] lg:h-[600px] overflow-hidden">
       {/* Hero Image */}
       <div className="relative w-full h-full flex items-center justify-center bg-gray-100">
         <Image
